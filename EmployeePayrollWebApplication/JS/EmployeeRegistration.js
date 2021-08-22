748d471715d8555ca1565f0bf6a29771c0b1b60d
ahamedsahib <adsahib39@gmail.com>
2021-08-22 17:15:13 +0530

    [Ahamed]Add:Checking Document loaded and then performing validation

--- EmployeePayrollWebApplication/JS/EmployeeRegistration.js
+++ EmployeePayrollWebApplication/JS/EmployeeRegistration.js
@@ -1,25 +1,59 @@
-//Salary range setter
-const salary = document.querySelector("#salary");
-const output = document.querySelector(".salary-output");
-output.textContent = salary.value;
-salary.addEventListener("input", function () {
-  output.textContent = salary.value;
-});
+window.addEventListener('DOMContentLoaded',(event)=>{
+    const name = document.querySelector("#name");
+    name.addEventListener('input',function(){
+        if(name.value.length == 0){
+               setTextValue('#errorName',"");
+               return;
+        }
+        try{
+            (new EmployeePayrollData()).empName= name.value;
+            setTextValue('#errorName',"");
+        }catch(e){
+            setTextValue('#errorName',e);
+        }
+    });
+    // salary range setter
+    const salary = document.querySelector("#salary");
+    const output = document.querySelector(".salary-output");
+    output.textContent = salary.value;
+    salary.addEventListener("input", function () {
+      output.textContent = salary.value;
+    });
+    //validates the date
+    const date = document.querySelector('.Date');
+    date.addEventListener('input',function(){
+        let startDate = getInputValue('day')+" "+getInputValue('month')+" "+getInputValue('year');
+        try{
+            (new EmployeePayrollData()).startDate = new Date(Date.parse(startDate));
+            setTextValue('.errorDate',"");
+        }catch(e){
+            setTextValue('.errorDate',e);
+        }
+    })
+  });
 
-
-const onSubmit=()=>{
-    let employee = new EmployeePayrollData();
+//onSubmit validates this function
+const EmployeePayroll=()=>{
+    let employee=new EmployeePayrollData;
     try{
-    employee.empName= document.getElementById("name").value;
+    employee.empName= getInputValue('name');
     employee.profilePic=getSelectedValues('[name = profile]').pop();
-    employee.gender=getSelectedValues('[name = gender]').pop();;
-    employee.salary = document.getElementById("salary").value;
-    employee.startDate=document.getElementById("day").value+" "+document.getElementById("month").value+" "+document.getElementById("year").value;
+    employee.salary = getInputValue("salary");
+    let date = getInputValue('day')+" "+getInputValue('month')+" "+getInputValue('year');
+    employee.Gender = getSelectedValues('[name = gender]').pop();
+    employee.Department = getSelectedValues('[name = department]');
+    employee.EmployeeNotes = getInputValue("notes");
+    employee.startDate=new Date(date);
+    try{
+    employee.startDate = new Date(Date.parse(date));
+    }catch(e){
+        setTextValue('.errorDate',e);
+        throw e;
+    }
     alert(employee.toString());
         
 }catch(e){
         alert(e);
-        console.log(employee.empName);
     }
    
 };
@@ -32,4 +66,12 @@ const getSelectedValues = (values)=>{
         selectedItem.push(item.value);
     });
     return selectedItem;
+}
+const setTextValue=(id,value)=>{
+    const attribute = document.querySelector(id);
+    attribute.textContent=value;
+}
+const getInputValue = (id)=>{
+    let value = document.getElementById(id).value;
+    return value;
 }
